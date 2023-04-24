d7e70cfaed3d1b843f43a076454f91da8ad89e02
root <root@ip-172-31-40-89.ap-east-1.compute.internal>
2023-04-24 02:51:13 +0000

    backup

--- backup.sh
+++ backup.sh
@@ -4,5 +4,7 @@ git add .
 git commit -m 'backup'
 git push github
 git push gitee
-#rclone sync -P --exclude '.git/' /var/www/html/ md:html/
+rclone sync -P --exclude '.git/' /var/www/html/ md:html/
+rclone sync -P --exclude '.git/' /var/www/html/ gd:html/
+
 echo "backup successfully!"
